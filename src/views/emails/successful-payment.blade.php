50bb952592501cddacade0d5cc5a35dd6fa5f4fd
Eduardo Mejia <edumejia30@gmail.com>
2020-04-05 14:35:35 -0400

    fix

--- src/views/emails/successful-payment.blade.php
+++ src/views/emails/successful-payment.blade.php
@@ -1,14 +1,12 @@
 @extends('master::layouts/email')
 
-@section('icon')
-Money
-@endsection
+@section('icon','Money')
 
 @section('content')
-	<h2 style="font-family: Arial, Helvetica, sans-serif;margin-top: 16px;margin-bottom: 8px;word-break: break-word;font-size: 28px;line-height: 38px;font-weight: bold;">
+	<h2 style="font-family: Arial, Helvetica, sans-serif;margin-top: 16px;margin-bottom: 16px;word-break: break-word;font-size: 28px;line-height: 38px;font-weight: bold;">
 		Pago Recibido
 	</h2>
-	<p style="font-family: Arial, Helvetica, sans-serif;margin-top: 0px;margin-bottom: 32px;word-break: break-word;font-size: 19px;line-height: 31px;">
+	<p style="font-family: Arial, Helvetica, sans-serif;margin-top: 16px;margin-bottom: 8px;word-break: break-word;font-size: 19px;line-height: 31px;">
 		{{ trans('payments::mail.successful_payment_content') }}
 	</p>
 @endsection
