565c3c7243058aacf1a491d442a188b33a033206
Eduardo Mejia <edumejia30@gmail.com>
2020-04-11 08:44:27 -0400

    fix

--- src/app/Controllers/Api/PagosttController.php
+++ src/app/Controllers/Api/PagosttController.php
@@ -93,7 +93,7 @@ class PagosttController extends BaseController {
                     $customer = \Customer::getCustomer($transaction->customer_id);
                 }
                 \Log::info('Successful Transaction Email: '.$customer['email']);
-                if(!config('payments.pagostt_params.testing')&&filter_var($customer['email'], FILTER_VALIDATE_EMAIL)){
+                if(!config('payments.pagostt_params.testing')&&config('payments.pagostt_params.notify_email')&&filter_var($customer['email'], FILTER_VALIDATE_EMAIL)){
                     \Mail::send('payments::emails.successful-payment', ['amount'=>$transaction->amount, 'email'=>$customer['email']], function($m) use($customer) {
                         if($customer['name']){
                             $name = $customer['name'];
